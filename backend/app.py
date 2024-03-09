4e9c813ac989155cc3d9dcb9d71cfa80435efb5a
Kyuupid <kittipongprasompong@gmail.com>
2024-03-10 03:05:55 +0700

    refactor: merge /movies into /movie & re-name vars

--- backend/app.py
+++ backend/app.py
@@ -7,69 +7,12 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/movies", methods=["GET"])
-def get_movies_endpoint():
-    conn = get_database_connection()
-    cursor = conn.cursor(buffered=True)
-    
-    query_combined = """
-    SELECT
-        M.movie_id,
-        JSON_OBJECT(
-            'title', M.movie_title,
-            'overview', M.movie_overview,
-            'img_url', M.movie_img_url,
-            'rating', M.movie_rating,
-            'popular', M.movie_popular,
-            'release_date', M.movie_release_date,
-            'genres', JSON_ARRAYAGG(
-                JSON_OBJECT(
-                    'id', G.genre_id,
-                    'title', G.genre_title
-                )
-            ),
-            'yt_video_ids', (SELECT JSON_ARRAYAGG(YT.trailer_id)
-                             FROM Trailer YT
-                             WHERE YT.movie_id = M.movie_id)
-        ) AS details
-    FROM
-        Movie AS M
-    LEFT JOIN
-        Movie_has_Genre AS MG 
-        ON M.movie_id = MG.movie_id
-    LEFT JOIN
-        Genre AS G 
-        ON G.genre_id = MG.genre_id
-    GROUP BY
-        M.movie_id,
-        M.movie_title,
-        M.movie_overview,
-        M.movie_img_url,
-        M.movie_rating,
-        M.movie_popular,
-        M.movie_release_date;
-    """
-    cursor.execute(query_combined)
-    result_set = cursor.fetchall()
-
-    movies = []
-    for row in result_set:
-        movie_data = {
-            "id": row[0],
-            "details": json.loads(row[1])
-        }
-        movies.append(movie_data)
-
-    commit_and_close(conn)
-    return jsonify({"results": movies})
-
-
 @app.route("/crews", methods=["GET"])
 def get_crews_endpoint():
     conn = get_database_connection()
     cursor = conn.cursor(buffered=True)
     
-    query_combined = """
+    sqlquery = """
     SELECT
         M.movie_id,
         JSON_OBJECT(
@@ -106,27 +49,27 @@ def get_crews_endpoint():
     GROUP BY
         M.movie_id
     """
-    cursor.execute(query_combined)
+    cursor.execute(sqlquery)
     result_set = cursor.fetchall()
 
-    crews = [] 
+    data = []
     for row in result_set:
         movie_data = {
-            "id": row[0],
+            "movie_id": row[0],
             "crews": json.loads(row[1])
         }
         
-        crews.append(movie_data) 
+        data.append(movie_data) 
 
     commit_and_close(conn)
-    return jsonify({"results": crews})
+    return jsonify({"results": data})
 
 @app.route("/actors", methods=["GET"])
 def get_actors_endpoint():
     conn = get_database_connection()
     cursor = conn.cursor(buffered=True)
     
-    query_combined = """
+    sqlquery = """
     SELECT
         M.movie_id,
         JSON_ARRAYAGG(
@@ -149,13 +92,13 @@ def get_actors_endpoint():
     GROUP BY
         M.movie_id;
     """
-    cursor.execute(query_combined)
+    cursor.execute(sqlquery)
     result_set = cursor.fetchall()
 
-    actors = [] 
+    data = [] 
     for row in result_set:
         movie_data = {
-            "id": row[0],
+            "movie_id": row[0],
             "actors": [] 
         }
 
@@ -171,108 +114,169 @@ def get_actors_endpoint():
             }
             movie_data["actors"].append(actor)
 
-        actors.append(movie_data) 
+        data.append(movie_data) 
 
     commit_and_close(conn)
-    return jsonify({"results": actors}) 
+    return jsonify({"results": data}) 
 
 @app.route("/movie", methods=["GET"])
 def get_movie_by_id():
     tmdb_id = request.args.get('tmdb_id', default=None, type=int)
-   
-    if tmdb_id is None:
-        return jsonify({"error": "Movie ID is cannot be None."})
     
     conn = get_database_connection()
     cursor = conn.cursor(buffered=True)
-    
-    query_combined = """
-    SELECT 
-        JSON_OBJECT (
-            'id', M.movie_id,    
-            'title', M.movie_title,
-            'overview', M.movie_overview,
-            'img_url', M.movie_img_url,
-            'rating', M.movie_rating,
-            'popular', M.movie_popular,
-            'release_date', M.movie_release_date,
-            'genres', (
-                SELECT JSON_ARRAYAGG(
+   
+    if tmdb_id is None:
+        sqlquery = """
+        SELECT
+            M.movie_id,
+            M.movie_title,
+            M.movie_img_url,
+            M.movie_rating,
+            M.movie_popular,
+            M.movie_release_date,
+            (SELECT 
+                JSON_ARRAYAGG(
                     JSON_OBJECT(
                         'id', G.genre_id,
                         'title', G.genre_title
                     )
                 )
-                FROM Movie_has_Genre AS MG
-                JOIN Genre AS G ON MG.genre_id = G.genre_id
-                WHERE MG.movie_id = M.movie_id
-            ),
-            'trailer_id', (
-                SELECT JSON_ARRAYAGG(
-                    JSON_OBJECT(
-                        'id', YT.trailer_id
-                    )
+            FROM Movie_has_Genre AS MG
+            LEFT JOIN Genre AS G ON G.genre_id = MG.genre_id
+            WHERE MG.movie_id = M.movie_id) AS genres
+        FROM
+            Movie AS M
+        GROUP BY
+            M.movie_id,
+            M.movie_title,
+            M.movie_img_url,
+            M.movie_rating,
+            M.movie_release_date;
+        """
+        cursor.execute(sqlquery)
+        result_set = cursor.fetchall()
+
+        data = []
+        for row in result_set:
+            movie_data = {
+                "id": row[0],
+                "title": row[1],
+                "img_url": row[2],
+                "rating": row[3] if row[3] is not None else 0,
+                "popular": row[4] if row[4] is not None else 0,
+                "release_date": row[5],
+                "genres": json.loads(row[6]) if row[6] is not None else None,
+            }
+            data.append(movie_data)
+
+        commit_and_close(conn)
+        return jsonify({"results": data})
+    
+    sqlquery = """
+    SELECT 
+        M.movie_id,    
+        M.movie_title,
+        M.movie_overview,
+        M.movie_img_url,
+        M.movie_rating,
+        M.movie_popular,
+        M.movie_release_date,
+        (SELECT 
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id', G.genre_id,
+                    'title', G.genre_title
                 )
-                FROM Trailer AS YT
-                WHERE YT.movie_id = M.movie_id
-            ),
-            'directors', (
-                SELECT JSON_ARRAYAGG(
-                    JSON_OBJECT(
-                        'id', D.director_id,
-                        'fname', D.director_fname,
-                        'lname', D.director_lname,
-                        'mname', D.director_mname,
-                        'img_url', D.director_img_url,
-                        'popular', D.director_popular
-                    )
-                )
-                FROM Director AS D
-                WHERE D.director_id = M.director_id
-            ),
-            'actors', (
-                SELECT JSON_ARRAYAGG(
-                    JSON_OBJECT(
-                        'fname', A.actor_fname,
-                        'lname', A.actor_lname,
-                        'mname', A.actor_mname,
-                        'img_url', A.actor_img_url,
-                        'popular', A.actor_popular
-                    )
-                )
-                FROM Movie_has_Actor AS MA
-                JOIN Actor AS A ON MA.actor_id = A.actor_id
-                WHERE MA.movie_id = M.movie_id
-            ),
-            'writers', (
-                SELECT JSON_ARRAYAGG(
-                    JSON_OBJECT(
-                        'id', W.writer_id,
-                        'fname', W.writer_fname,
-                        'lname', W.writer_lname,
-                        'mname', W.writer_mname,
-                        'img_url', W.writer_img_url,
-                        'popular', W.writer_popular
-                    )
-                )
-                FROM Movie_has_Writer AS MW
-                JOIN Writer AS W ON MW.writer_id = W.writer_id
-                WHERE MW.movie_id = M.movie_id
             )
-        ) AS results
+        FROM Movie_has_Genre AS MG
+        LEFT JOIN Genre AS G ON G.genre_id = MG.genre_id
+        WHERE MG.movie_id = M.movie_id) AS genres,
+        (SELECT 
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id', T.trailer_id
+                )
+            )
+        FROM Trailer AS T
+        WHERE T.movie_id = M.movie_id) AS trailerd,
+        (SELECT
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id', D.director_id,
+                    'fname', D.director_fname,
+                    'lname', D.director_lname,
+                    'mname', D.director_mname,
+                    'img_url', D.director_img_url,
+                    'popular', D.director_popular
+                )
+            )
+        FROM Director AS D
+        WHERE D.director_id = M.director_id) AS directors,
+        (SELECT 
+            JSON_ARRAYAGG(
+                JSON_OBJECT( 
+                    'id', A.actor_id,
+                    'fname', A.actor_fname,
+                    'lname', A.actor_lname,
+                    'mname', A.actor_mname,
+                    'img_url', A.actor_img_url,
+                    'popular', A.actor_popular
+                )
+            )
+        FROM Movie_has_Actor AS MA
+        LEFT JOIN Actor AS A ON A.actor_id = MA.actor_id
+        WHERE MA.movie_id = M.movie_id) AS actors,
+        (SELECT
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id', W.writer_id,
+                    'fname', W.writer_fname,
+                    'lname', W.writer_lname,
+                    'mname', W.writer_mname,
+                    'img_url', W.writer_img_url,
+                    'popular', W.writer_popular
+                )
+            )
+        FROM Movie_has_Writer AS MW
+        LEFT JOIN Writer AS W ON W.writer_id = MW.writer_id
+        WHERE MW.movie_id = M.movie_id) AS writers
     FROM Movie AS M
     WHERE M.movie_id = %s
+    GROUP BY
+        M.movie_id,
+        M.movie_title,
+        M.movie_overview,
+        M.movie_img_url,
+        M.movie_rating,
+        M.movie_popular,
+        M.movie_release_date;
     """
     
-    cursor.execute(query_combined, (tmdb_id,))
+    cursor.execute(sqlquery, (tmdb_id,))
     result_set = cursor.fetchall()
     
     data = []
     for row in result_set:
-        data.append(json.loads(row[0]))
+        movie_data = {
+            "id": row[0],
+            "title": row[1],
+            "overview": row[2],
+            "img_url": row[3],
+            "rating": row[4] if row[4] is not None else 0,
+            "popular": row[5] if row[5] is not None else 0,
+            "release_date": row[6],
+            "genres": json.loads(row[7]) if row[7] is not None else None,
+            "trailer": json.loads(row[8]) if row[8] is not None else None,
+            "directors": json.loads(row[9]),
+            "actors": json.loads(row[10]),
+            "writers": json.loads(row[11])
+        }
+        data.append(movie_data)
 
     commit_and_close(conn)
     return jsonify({"results": data})
+
 
 @app.route("/updb", methods=["GET"])
 def update_database_endpoint():
